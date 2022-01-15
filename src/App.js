8bec840269f039d04d18aa50089dba517c441414
Sajan M P <sajanmp@gmail.com>
2022-01-15 23:06:46 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [firstName,setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
   return (
     <div className="App">
       <label>First Name</label>
-     <input name="first_name"></input>
+     <input name="first_name" value={firstName} onChange={e=>setFirstName(e.target.value)}></input>
      <br/>
      <label>Last Name</label>
-     <input name="last_name"></input>
+     <input name="last_name" value={lastName} onChange={e=>setLastName(e.target.value)}></input>
+     <br/>
+     <label>Welcome {firstName} {lastName}</label>
     </div>
   );
 }
