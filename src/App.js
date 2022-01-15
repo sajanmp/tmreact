88b96248adf0ee9c95576ac206204be6e06edf95
Sajan M P <sajanmp@gmail.com>
2022-01-15 22:58:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>First Name</label>
+     <input name="first_name"></input>
+     <br/>
+     <label>Last Name</label>
+     <input name="last_name"></input>
     </div>
   );
 }
